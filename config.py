3efbad1fd71d7cc732d0e271e41431181301ada8
mr_mp3 <107989942+mr-mp3@users.noreply.github.com>
2025-12-26 01:12:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8443877498:AAFDoZSnzzQksrEHESwlj6qqQ2aFiK60JSs")
-KINOPOISK_API_KEY = os.getenv("KINOPOISK_API_KEY", "TCPD5P5-X794MKP-GKFBCMT-08107MS")
+KINOPOISK_API_KEY = os.getenv("KINOPOISK_API_KEY", "AD79R6V-26WM6Y5-PJTYAYC-D3SF1HG")
 
-KINOPOISK_API_URL = 'https://api.kinopoisk.dev/v1.4'
+KINOPOISK_API_URL = "https://api.poiskkino.dev/v1.4"
 
 GENRES = [
     "комедия", "драма", "боевик", "фантастика", "ужасы",
