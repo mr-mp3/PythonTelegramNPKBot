2bd2dd8b35e9eb4e66dba9820df44596ab506c21
mr_mp3 <107989942+mr-mp3@users.noreply.github.com>
2026-01-22 00:18:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8443877498:AAFDoZSnzzQksrEHESwlj6qqQ2aFiK60JSs")
-KINOPOISK_API_KEY = os.getenv("KINOPOISK_API_KEY", "AD79R6V-26WM6Y5-PJTYAYC-D3SF1HG")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "...")
+KINOPOISK_API_KEY = os.getenv("KINOPOISK_API_KEY", "...")
 
 KINOPOISK_API_URL = "https://api.poiskkino.dev/v1.4"
 
